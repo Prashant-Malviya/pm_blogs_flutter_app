8623c162478f9dec29fee8f04ff7ad3aab9a33c0
Prashant Malviya <prashantmalviya272002@gmail.com>
2025-05-11 06:47:17 +0530

    basic flutter app created

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:pm_blog_app/core/theme/theme.dart';
+import 'package:pm_blog_app/features/auth/presentation/pages/signup_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'PM Blogs',
+      theme: AppTheme.darkThemeMode,
+      home: const SignUpPage(),
+    );
+  }
+}
