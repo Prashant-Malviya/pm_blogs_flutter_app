3b338d0671da9154708c18c0e5ef7ccf1fb45f0e
Prashant Malviya <prashantmalviya272002@gmail.com>
2025-05-11 11:12:19 +0530

    signup page created

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pm_blog_app/features/auth/presentation/widgets/auth_field.dart';
+import 'package:pm_blog_app/features/auth/presentation/widgets/auth_gradient_button.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -12,15 +13,24 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Sign Up.',
-            style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-          ),
-          AuthField(hintText: 'Email'),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Sign Up.',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 30),
+            AuthField(hintText: 'Email'),
+            const SizedBox(height: 30),
+            AuthField(hintText: 'Name'),
+            const SizedBox(height: 30),
+            AuthField(hintText: 'Password'),
+            AuthGradientButton(),
+          ],
+        ),
       ),
     );
   }
